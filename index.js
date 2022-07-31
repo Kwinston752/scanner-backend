3588c3c7a40b6cd014a57a64a81064a21fa0a3c6
Kwinston <yalikhan752@gmail.com>
2022-08-01 04:37:26 +0600

    0.1.3

--- index.js
+++ index.js
@@ -1,85 +1,26 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const cors = require('cors')
-const axios = require('axios')
+const axios = require('axios');
+const { json } = require('body-parser');
 
 
 const app = express()
 app.use(cors())
+app.use(bodyParser.urlencoded({
+  extended: true
+}))
+app.use(bodyParser.json())
 const port = process.env.PORT || 5050;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+app.post('/binance/search', async (req, res) => {
 
-const getBankRate = async (payType, tradeType) => {
-  const res = await axios({
+  const response = await axios({
     method: 'post',
     url: 'https://p2p.binance.com/bapi/c2c/v2/friendly/c2c/adv/search',
-    data: {
-      page: 1,
-      rows: 1,
-      payTypes: [payType],
-      countries: [],
-      publisherType: null,
-      fiat: "KZT",
-      tradeType: tradeType,
-      asset: "USDT",
-      transAmount: "5000",
-      merchantCheck: false
-    }
+    data: req.body
   })
-
-  return res.data.data[0]?.adv.price
-}
-let data = {}
-
-
-const fetchRates = async () => {
-  const banks = ['Kaspi Bank', 'Halyk Bank', 'Jysan Bank', 'Forte Bank', 'Eurasian Bank', 'CenterCredit Bank', 'Altyn Bank', 'AdvCash', 'QIWI']
-  let buyPrices = []
-  let sellPrices = []
-
-  let response = {}
-
-  await Promise.all([
-    getBankRate('KaspiBank', 'BUY'),
-    getBankRate('HalykBank', 'BUY'),
-    getBankRate('JysanBank', 'BUY'),
-    getBankRate('ForteBank', 'BUY'),
-    getBankRate('EurasianBank', 'BUY'),
-    getBankRate('CenterCreditBank', 'BUY'),
-    getBankRate('AltynBank', 'BUY'),
-    getBankRate('AdvCash', 'BUY'),
-    getBankRate('QIWI', 'BUY'),
-  ]).then(rates => buyPrices = rates)
-
-  await Promise.all([
-    getBankRate('KaspiBank', 'SELL'),
-    getBankRate('HalykBank', 'SELL'),
-    getBankRate('JysanBank', 'SELL'),
-    getBankRate('ForteBank', 'SELL'),
-    getBankRate('EurasianBank', 'SELL'),
-    getBankRate('CenterCreditBank', 'SELL'),
-    getBankRate('AltynBank', 'SELL'),
-    getBankRate('AdvCash', 'SELL'),
-    getBankRate('QIWI', 'SELL'),
-  ]).then(rates => sellPrices = rates)
-
-  for (let i in banks) {
-    response[banks[i]] = {
-      buy: buyPrices[i],
-      sell: sellPrices[i]
-    }
-  }
-
-  data = response
-}
-
-setInterval(() => fetchRates(), 60000)
-fetchRates()
-
-app.get('/binance/rates', async (req, res) => {
-  res.json(data)
+  res.json(response.data)
 })
 
 app.listen(port, () => {
